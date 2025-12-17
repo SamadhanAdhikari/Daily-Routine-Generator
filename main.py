157acb522cb0e38a1b3685351ecd15169d6c67a3
Samadhan Adhikari <adhikarisamadhan@gmail.com>
2025-12-17 14:27:53 +0545

    Add result window functionality and improve UI layout for task display

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.daily_tasks = []
+        self.result_window = None
         self.read_task = QLineEdit(self)
         self.read_time = QLineEdit(self)
         self.read_duration = QLineEdit(self)
@@ -29,62 +30,68 @@ class MainWindow(QWidget):
         self.instruction.setObjectName("instruction")
         self.submit_button.setObjectName("submit_button")
         self.routine_button.setObjectName("routine_button")
-        self.result_window = None
 
         self.setStyleSheet("""
-            MainWindow {
-                    background-color: #222831;
-                           }
+            QWidget {
+                background-color: #222831;
+            }
 
             QLabel#HEADER {
-                           font-size: 50px;
-                           font-family: Bold Font;
-                           font-weight: Bold;
-                           background-color: #393E46;
-                           padding: 20px;
-                           border-radius: 20px;
-                           margin-bottom: 10px;
-                           color: #EBD5AB;
-                           }
+                font-size: 50px;
+                font-family: Bold Font;
+                font-weight: Bold;
+                background-color: #393E46;
+                padding: 20px;
+                border-radius: 20px;
+                margin-bottom: 10px;
+                color: #EBD5AB;
+            }
 
             QLineEdit#read_task {
-                           padding: 15px;
-                           border-radius: 15px;
-                           font-size: 25px;
-                           margin-bottom: 15px;
-                           color: #00ADB5;
-                           }
+                padding: 15px;
+                border-radius: 15px;
+                font-size: 25px;
+                margin-bottom: 15px;
+                background-color: #00ADB5;
+                color: #222831;
+            }
 
             QLineEdit#read_time {
-                           padding: 15px;
-                           border-radius: 15px;
-                           font-size: 25px;
-                           margin-bottom: 15px;
-                           color: #00ADB5;
-                           }
+                padding: 15px;
+                border-radius: 15px;
+                font-size: 25px;
+                margin-bottom: 15px;
+                background-color: #00ADB5;
+                color: #222831;
+            }
 
             QLineEdit#read_duration {
-                           padding: 15px;
-                           border-radius: 15px;
-                           font-size: 25px;
-                           margin-bottom: 25px;
-                           color: #00ADB5;
-                           }
+                padding: 15px;
+                border-radius: 15px;
+                font-size: 25px;
+                margin-bottom: 25px;
+                background-color: #00ADB5;
+                color: #222831;
+            }
 
             QPushButton#submit_button {
-                           padding: 15px;
-                           background-color: whitesmoke;
-                           border-radius: 25px;
-                           font-size: 40px;
-                           font-family: Times New Roman;
-                           }
+                padding: 15px;
+                background-color: whitesmoke;
+                border-radius: 25px;
+                font-size: 40px;
+                font-family: Times New Roman;
+            }
+
+            QPushButton#submit_button:hover {
+                background-color: #d3d3d3;
+            }
 
             QLabel#instruction {
-                           margin-bottom: 10px;
-                           color: #EBD5AB;
-                           font-family: Calibri;
-                           font-size: 30px;
-                           }
+                margin-bottom: 10px;
+                color: #EBD5AB;
+                font-family: Calibri;
+                font-size: 30px;
+            }
 
             QPushButton#routine_button {
                 padding: 20px;
@@ -95,6 +102,10 @@ class MainWindow(QWidget):
                 font-size: 30px;
                 border-radius: 15px;
                 font-weight: Bold;
+            }
+
+            QPushButton#routine_button:hover {
+                background-color: #00ADB5;
             }
         """)
         
@@ -112,13 +123,12 @@ class MainWindow(QWidget):
         self.routine_button.clicked.connect(self.show_ResultWindow)
 
     def get_tasks(self):
-
         task = self.read_task.text()
         time = self.read_time.text()
         duration = self.read_duration.text()
 
-        if self.read_task.text() != "" and self.read_time.text() != "" and self.read_duration.text() != "":
-            self.daily_tasks.append({task, time, duration})
+        if task != "" and time != "" and duration != "":
+            self.daily_tasks.append({"task": task, "time": time, "duration": duration})
             self.read_task.clear()
             self.read_time.clear()
             self.read_duration.clear()
@@ -136,9 +146,88 @@ class MainWindow(QWidget):
 class ResultWindow(QWidget):
     def __init__(self, tasks):
         super().__init__()
-        self.HEADER = QLabel("RESULT", self)
+        self.tasks = tasks
+        self.HEADER = QLabel("DAILY ROUTINE", self)
+        self.table = QTableWidget()
+        self.initUI()
 
+    def initUI(self):
+        self.setWindowTitle("Daily Routine")
+        self.move(300, 30)
+        self.resize(800, 600)
 
+        self.HEADER.setObjectName("HEADER")
+        self.table.setObjectName("routine_table")
+        
+        # Set table structure
+        self.table.setColumnCount(3)  # Added this line
+        self.table.setHorizontalHeaderLabels(["Task", "Time", "Duration"])
+
+        self.table.setColumnWidth(0, 250)
+        self.table.setColumnWidth(1, 200)
+        self.table.setColumnWidth(2, 200)
+
+        self.populate_table()
+        
+        self.setStyleSheet("""
+            QWidget {
+                background-color: #222831;
+            }
+
+            QLabel#HEADER {
+                font-size: 50px;
+                font-family: Bold Font;
+                font-weight: Bold;
+                background-color: #393E46;
+                padding: 20px;
+                border-radius: 20px;
+                margin-bottom: 20px;
+                color: #EBD5AB;
+            }
+
+            QTableWidget#routine_table {
+                background-color: #393E46;
+                gridline-color: #00ADB5;
+                font-size: 18px;
+                border-radius: 10px;
+            }
+
+            QTableWidget#routine_table::item {
+                padding: 10px;
+                color: #EBD5AB;
+            }
+
+            QTableWidget#routine_table::item:selected {
+                background-color: #00ADB5;
+                color: #222831;
+            }
+
+            QHeaderView::section {
+                background-color: #00ADB5;
+                color: #222831;
+                padding: 10px;
+                font-weight: bold;
+                font-size: 20px;
+                border: none;
+            }
+        """)
+        
+        vbox = QVBoxLayout()  # Added parentheses
+        vbox.addWidget(self.HEADER, alignment=Qt.AlignmentFlag.AlignCenter)
+        vbox.addWidget(self.table)
+        self.setLayout(vbox)
+
+    def populate_table(self):
+        self.table.setRowCount(len(self.tasks))
+        
+        for row, task_dict in enumerate(self.tasks):
+            self.table.setItem(row, 0, QTableWidgetItem(task_dict["task"]))
+            self.table.setItem(row, 1, QTableWidgetItem(task_dict["time"]))
+            self.table.setItem(row, 2, QTableWidgetItem(task_dict["duration"]))
+
+    def update_tasks(self, tasks):
+        self.tasks = tasks
+        self.populate_table()
 
 def main():
     app = QApplication(sys.argv)
